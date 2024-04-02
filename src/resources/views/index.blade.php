23aeb67fb5dbb28e6350783c4c65dc9336aa02e3
sasaki minaho <m37h0kl.u@gmail.com>
2024-04-02 15:35:11 +0900

    タブメニュー作成

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -5,5 +5,20 @@
 @endsection
 
 @section('content')
-
+<div class="container">
+    <input type="radio" name="radio" id="tab1" checked>
+    <input type="radio" name="radio" id="tab2">
+    <div class="tabs">
+        <label for="tab1" class="tab1">おすすめ</label>
+        <label for="tab2" class="tab2">マイリスト</label>
+    </div>
+    <div class="contents">
+        <div class="content1">
+            おすすめの商品が表示されます。
+        </div>
+        <div class="content2">
+            お気に入り登録した商品が表示されます。
+        </div>
+    </div>
+</div>
 @endsection
--- src/resources/views/my_page.blade.php
+++ src/resources/views/my_page.blade.php
@@ -8,15 +8,30 @@
 <div class="my_page">
     <div class="user_information">
         <div class="user_image">
-            <img src="" alt="">
+            <img src="" alt="" class="image">
             <div class="user_name">ユーザー名</div>
-            <div class="profile_button">
-                <button class="profile_change">
-                    プロフィールを編集
-                </button>
-            </div>
+        </div>
+        <div class="profile_button">
+            <button class="profile_change">
+                プロフィールを編集
+            </button>
         </div>
     </div>
-    
+    <div class="container">
+    <input type="radio" name="radio" id="tab1" checked>
+    <input type="radio" name="radio" id="tab2">
+    <div class="tabs">
+        <label for="tab1" class="tab1">出品した商品</label>
+        <label for="tab2" class="tab2">購入した商品</label>
+    </div>
+    <div class="contents">
+        <div class="content1">
+            出品した商品が表示されます。
+        </div>
+        <div class="content2">
+            購入した商品が表示されます。
+        </div>
+    </div>
+</div>
 </div>
 @endsection
